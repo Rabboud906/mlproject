1ff8f3234681a3272db9c56f0b42da0c35bd865e
Rawad Abboud <rawadbbd@gmail.com>
2026-02-27 15:07:25 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setup.tools import setup, find_packages
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path) -> List[str]:
+  """This function reads the requirements.txt file and returns a list of requirements."""
+  requirements = []
+  with open(file_path) as file:
+      requirements = file.readlines()
+      requirements = [req.replace('\n', " ") for req in requirements]
+
+      if HYPEN_E_DOT in requirements:
+          requirements.remove(HYPEN_E_DOT)
+  return requirements
+
+
+
+setup(
+  name = "mlproject", 
+  version = "0.1.0", 
+  author = "Rawad Abboud",
+  author_email = "rawadbbd@gmail.com",
+  packages = find_packages(),
+  install_requires = get_requirements('requirements.txt')
+)
